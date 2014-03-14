48784812feeebf5d646c72fb058a75d100981f31
Robert Fryman <robert.fryman@gmail.com>
2014-03-14 13:49:05 -0500

    user unit done

---
+++ app/models/user.js
@@ -0,0 +1,134 @@
+'use strict';
+
+module.exports = User;
+var bcrypt = require('bcrypt');
+var users = global.nss.db.collection('users');
+var email = require('../lib/email');
+var path = require('path');
+var fs = require('fs');
+var Mongo = require('mongodb');
+
+/* ---------------------------------- *
+ * User
+ * _id
+ * email
+ * password
+ * role
+ *
+ * #register
+ * .findByEmailAndPassword
+ * ---------------------------------- */
+
+function User(user){
+  this.name = user.name;
+  this.email = user.email;
+  this.password = user.password;
+  this.pic = user.pic ? user.pic : null;
+  this.nodeBucks = user.nodeBucks * 1;
+  this.lastLogin = new Date();
+}
+
+User.prototype.register = function(fn){
+  var self = this;
+
+  hashPassword(self.password, function(hashedPwd){
+    self.password = hashedPwd;
+    if(self.pic){
+      addPic(self.pic, function(path){
+        self.pic = path;
+      });
+    }
+    insert(self, function(err){
+      if(self._id){
+        email.sendWelcome({to:self.email}, function(err, body){
+          fn(err, body);
+        });
+      }else{
+        fn();
+      }
+    });
+  });
+};
+
+function addPic(oldpath, fn){
+  var filename = path.basename(oldpath);
+  var abspath = __dirname + '/../static';
+  var relpath = '/img/users/' + filename;
+
+  fs.renameSync(oldpath, abspath+relpath);
+
+  fn(relpath);
+}
+
+User.findById = function(id, fn){
+  var _id = Mongo.ObjectID(id);
+  users.findOne({_id:_id}, function(err, record){
+    fn(record);
+  });
+};
+
+User.findByEmailAndPassword = function(email, password, fn){
+  users.findOne({email:email}, function(err, user){
+    if(user){
+      bcrypt.compare(password, user.password, function(err, result){
+        if(result){
+          fn(user);
+        }else{
+          fn();
+        }
+      });
+    }else{
+      fn();
+    }
+  });
+};
+
+function insert(user, fn){
+  users.findOne({email:user.email}, function(err, userFound){
+    if(!userFound){
+      users.findOne({name:user.name}, function(err, userFound){
+        if(!userFound){
+          users.insert(user, function(err, record){
+            fn(err);
+          });
+        }else{
+          fn();
+        }
+      });
+    }else{
+      fn();
+    }
+  });
+}
+
+function update(user, fn){
+  users.update({_id:user._id}, user, function(err, count){
+    fn(err);
+  });
+}
+
+function hashPassword(password, fn){
+  bcrypt.hash(password, 8, function(err, hash){
+    fn(hash);
+  });
+}
+
+User.prototype.setHome = function(lat, lng, fn){
+  this.home = [parseFloat(lat), parseFloat(lng)];
+  update(this, function(err){
+    fn(err);
+  });
+};
+
+User.findAll = function(fn){
+  users.find().toArray(function(err, records){
+    fn(records);
+  });
+};
+
+User.findByName = function(name, fn){
+  users.findOne({name:name}, function(err, record){
+    fn(record);
+  });
+};
+
---
+++ test/unit/user_spec.js
@@ -0,0 +1,162 @@
+/* jshint expr:true */
+
+'use strict';
+/*
+process.env.DBNAME = 'nodemon-test';
+var expect = require('chai').expect;
+var Mongo = require('mongodb');
+var User;
+var bob;
+*/
+
+process.env.DBNAME = 'nodemon-test';
+var expect = require('chai').expect;
+var User;
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+
+describe('User', function(){
+
+  before(function(done){
+    var initMongo = require('../../app/lib/init-mongo');
+    initMongo.db(function(){
+      User = require('../../app/models/user');
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    var testdir = __dirname + '/../../app/static/img/users/test*';
+    var cmd = 'rm ' + testdir;
+
+    exec(cmd, function(){
+      var origfile = __dirname + '/../fixtures/testfile.jpg';
+      var copyfile = __dirname + '/../fixtures/testfile-copy.jpg';
+      var copyfile2 = __dirname + '/../fixtures/testfile2-copy.jpg';
+      fs.createReadStream(origfile).pipe(fs.createWriteStream(copyfile));
+      fs.createReadStream(origfile).pipe(fs.createWriteStream(copyfile2));
+      global.nss.db.dropDatabase(function(err, result){
+        var u1 = new User({name: 'Samuel', email:'sami1@nomail.com', password:'1234', nodeBucks:'5',lat:'0', lng:'0', pic: '/data/code/projects/javagotchi/test/fixtures/testfile2-copy.jpg'});
+        u1.register(function(){
+          done();
+        });
+      });
+    });
+  });
+
+  describe('new', function(){
+    it('should create a new User object', function(done){
+      var u1 = new User({name: 'Sam', email:'sam@nomail.com', password:'1234', nodeBucks:'5'});
+      expect(u1.email).to.equal('sam@nomail.com');
+      expect(u1.password).to.equal('1234');
+      expect(u1.nodeBucks).to.equal(5);
+      expect(u1.name).to.equal('Sam');
+      done();
+    });
+  });
+
+  describe('register', function(){
+    it('should register user', function(done){
+      var u1 = new User({name: 'Sam', email:'sam@nomail.com', password:'1234', nodeBucks:'5'});
+      u1.register(function(){
+        expect(u1.email).to.equal('sam@nomail.com');
+        expect(u1.password).to.have.length(60);
+        expect(u1.nodeBucks).to.equal(5);
+        expect(u1.name).to.equal('Sam');
+        expect(u1._id.toString()).to.have.length(24);
+        done();
+      });
+    });
+
+    it('should not register a user to the database for duplicate email', function(done){
+      var u1 = new User({name: 'Sam', email:'sam@nomail.com', password:'1234', nodeBucks:'5'});
+      var u2 = new User({name: 'Sam', email:'sami1@nomail.com', password:'1234', nodeBucks:'5'});
+      u1.register(function(){
+        u2.register(function(){
+          expect(u1.password).to.have.length(60);
+          expect(u1._id.toString()).to.have.length(24);
+          expect(u2._id).to.not.be.ok;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#setHome', function(){
+    it('should set the home coordinates of the user', function(done){
+      var u1 = new User({name: 'Sam', email:'sam@nomail.com', password:'1234', nodeBucks:'5'});
+      u1.register(function(){
+        u1.setHome('0', '0', function(){
+          expect(u1.home).to.deep.equal([0, 0]);
+          done();
+        });
+      });
+    });
+  });
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FIND METHODS >>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  describe('.findById', function(){
+    it('should find a user by id', function(done){
+      var u1 = new User({name: 'Sam', email:'sam@nomail.com', password:'1234', pic: '/data/code/projects/javagotchi/test/fixtures/testfile-copy.jpg'});
+      u1.register(function(){
+        User.findById(u1._id.toString(), function(record){
+          expect(u1.pic).to.equal('/img/users/testfile-copy.jpg');
+          expect(u1.email).to.equal('sam@nomail.com');
+          expect(u1.password).to.not.equal('1234');
+          expect(record.name).to.equal('Sam');
+          expect(record._id).to.deep.equal(u1._id);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.findByEmailAndPassword', function(){
+    it('should find a user', function(done){
+      User.findByEmailAndPassword('sami1@nomail.com', '1234', function(user){
+        expect(user).to.be.ok;
+        done();
+      });
+    });
+    it('should not find user - bad email', function(done){
+      User.findByEmailAndPassword('wrong@nomail.com', '1234', function(user){
+        expect(user).to.be.undefined;
+        done();
+      });
+    });
+    it('should not find user - bad password', function(done){
+      User.findByEmailAndPassword('sami1@nomail.com', 'wrong', function(user){
+        expect(user).to.be.undefined;
+        done();
+      });
+    });
+  });
+
+  describe('.findAll', function(){
+    it('should all users in the db', function(done){
+      var u2 = new User({name: 'Sam', email:'adam@nomail.com', password:'1234', nodeBucks:'5'});
+      u2.register(function(){
+        User.findAll(function(users){
+          expect(users.length).to.equal(2);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.findByName', function(){
+    it('should find users by name in the db', function(done){
+      var u2 = new User({name: 'Adam', email:'adam@nomail.com', password:'1234', nodeBucks:'5'});
+      u2.register(function(){
+        User.findByName('Adam', function(users){
+          expect(users.name).to.equal('Adam');
+          done();
+        });
+      });
+    });
+  });
+
+
+});
