73d0e685d3d1374c961c6c63ea7f8a29111cabd3
Nat Webb <natrwebb@gmail.com>
2014-03-17 09:34:25 -0500

    no meaningful changes

--- app/lib/agenda.js
+++ app/lib/agenda.js
@@ -5,7 +5,7 @@ var Agenda = require('agenda');
 exports.start = function(){
   var agenda = new Agenda();
   agenda.database('localhost:27017/nodemon-test', 'agendaJobs').processEvery('1 second');
-  agenda.every('5 seconds', 'get restless');
+  //agenda.every('5 seconds', 'get restless');
   agenda.start();
 };
 
@@ -13,11 +13,11 @@ exports.restless = function(petId){
   var agenda = new Agenda();
   agenda.database('localhost:27017/nodemon-test', 'agendaJobs').processEvery('1 second');
 
-  agenda.define('get restless', function(job){
+  agenda.define('get restless' + petId.toString(), function(job){
     console.log(petId.toString() + ' is restless! Time for a walk!');
   });
 
-  agenda.every('5 seconds', 'get restless');
+  agenda.every('5 seconds', 'get restless' + petId.toString());
 
   agenda.start();
 };
